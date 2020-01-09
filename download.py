1f1d2546235a473f8ef2752dc17a5b90d2933336
Anton Taleckij <anton.taleckij@gmail.com>
2020-01-10 01:33:43 +0300

    added tqdm output + lite improvements

--- download.py
+++ download.py
@@ -1,3 +1,4 @@
+from tqdm import tqdm
 from lxml import html
 import requests
 import urllib.request
@@ -7,20 +8,26 @@ import os.path
 
 HANDLE = '^/handle/[0-9]+/[0-9]+$'
 BASE_URL = 'https://tspace.library.utoronto.ca'
+DATASET_DIR = './data/'
+
+if not os.path.isdir(DATASET_DIR):
+    os.mkdir(DATASET_DIR)
+
 page = requests.get(BASE_URL + '/handle/1807/24487')
 tree = html.fromstring(page.content)
 subset = [ href.attrib['href'] for href in tree.xpath('//a') if re.match(HANDLE, href.attrib['href'])]
 
 for s in subset:
+    print(s)
     wav_page = requests.get(BASE_URL + s)
-    tree     = html.fromstring(wav_page.content)
-    links = [ href.attrib['href'] for href in tree.xpath('//a') if 'wav' in href.attrib['href']]
-    for link in links:
+    tree = html.fromstring(wav_page.content)
+    links = [href.attrib['href'] for href in tree.xpath('//a') if 'wav' in href.attrib['href']]
+    for link in tqdm(links):
         local = link.split('/')[-1]
-        if not os.path.isfile('data/' + local):
+        if not os.path.isfile('' + local):
             try:
                 urllib.request.urlretrieve(BASE_URL + link, 'data/' + local)
-                print('Download: ', link)
+                #print('Download: ', link)
             except IOError:
                 print('Err: ', link)
             time.sleep(1)
